ab3a43fa1b62ee48d43e4d91d452984717e7c40c
Kevin Casey <kacasey@fb.com>
2020-12-31 07:36:49 -0800

    modify procfile for heroku https://github.com/realpython/flask-boilerplate/issues/14

--- catalog/views.py
+++ catalog/views.py
@@ -30,14 +30,20 @@ def _get_current_price(stockticker_name):
     return yahoo_ticker_history.tail(1)['Close'].iloc[0]
 
 def _compute_put_stat(current_price, interesting_put, days_to_expiry, historical_volatility, expiration_date):
-    put_implied_volatility_calculator = mibian.BS([current_price, interesting_put.strike, INTEREST_RATE, days_to_expiry], putPrice=interesting_put.lastPrice)
+    strike, last_price, bid, ask = [interesting_put.strike, interesting_put.lastPrice, interesting_put.bid, interesting_put.ask]
+    effective_price = last_price
+    if (bid == 0 and ask == 0) == False:
+        # bid and ask will be 0 during off hours, so use last_price as an estimate.
+        # During trading hours we assume we'll get a price halfway between the bid and the ask
+        effective_price = round((bid + ask) / 2, 3)
+    put_implied_volatility_calculator = mibian.BS([current_price, strike, INTEREST_RATE, days_to_expiry], putPrice=effective_price)
     # kinda silly, we need to construct another object to extract delta for a computation based on real put price
     implied_volatility = put_implied_volatility_calculator.impliedVolatility # Yahoo's volatility in interesting_put.impliedVolatility seems low, ~20% too low
-    put_with_implied_volatility = mibian.BS([current_price, interesting_put.strike, INTEREST_RATE, days_to_expiry], volatility=implied_volatility)
-    max_profit_decimal = interesting_put.lastPrice / interesting_put.strike
+    put_with_implied_volatility = mibian.BS([current_price, strike, INTEREST_RATE, days_to_expiry], volatility=implied_volatility)
+    max_profit_decimal = effective_price / strike
     stats = {
-        "strike": interesting_put.strike,
-        "price": interesting_put.lastPrice,
+        "strike": strike,
+        "price": effective_price,
         "expiration_date": expiration_date,
         "days_to_expiry": days_to_expiry,
         # https://www.macroption.com/delta-calls-puts-probability-expiring-itm/ "Option’s delta as probability proxy"
@@ -48,12 +54,12 @@ def _compute_put_stat(current_price, interesting_put, days_to_expiry, historical
 
     if False:
         # these extra computations are interesting, but take extra processing time
-        put = mibian.BS([current_price, interesting_put.strike, INTEREST_RATE, days_to_expiry], volatility=historical_volatility)
-        put_with_premium = mibian.BS([current_price, interesting_put.strike - interesting_put.lastPrice, INTEREST_RATE, days_to_expiry], volatility=historical_volatility)
-        put_with_premium_with_implied_volatility = mibian.BS([current_price, interesting_put.strike - interesting_put.lastPrice, INTEREST_RATE, days_to_expiry], volatility=implied_volatility)
+        put = mibian.BS([current_price, strike, INTEREST_RATE, days_to_expiry], volatility=historical_volatility)
+        put_with_premium = mibian.BS([current_price, strike - effective_price, INTEREST_RATE, days_to_expiry], volatility=historical_volatility)
+        put_with_premium_with_implied_volatility = mibian.BS([current_price, strike - effective_price, INTEREST_RATE, days_to_expiry], volatility=implied_volatility)
         stats.update({
             "historical_expected_put_price": put.putPrice,
-            "actual_put_price": interesting_put.lastPrice,
+            "actual_put_price": effective_price,
             "decimal_odds_profitable_historical": 1 + put_with_premium.putDelta,
             "decimal_odds_profitable_implied": 1 + put_with_premium_with_implied_volatility.putDelta,
             "decimal_odds_in_the_money_historical": 1 + put.putDelta,
