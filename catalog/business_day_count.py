386cd1596dc0a541cdbc8e36d95822687b9f56c9
Kevin Casey <kacasey@fb.com>
2021-01-17 15:55:44 -0800

    patch business days

--- catalog/business_day_count.py
+++ catalog/business_day_count.py
@@ -5,6 +5,8 @@ from pandas.tseries.holiday import USFederalHolidayCalendar
 # end days are included
 def busday_count_inclusive(start_date, end_date):
   C = pandas.offsets.CustomBusinessDay(calendar=USFederalHolidayCalendar())
+  if (start_date == end_date):
+  	return 1
   pandas_result = (pandas.DataFrame(index=pandas.to_datetime([start_date, end_date]))
       .resample(C, closed='right') 
       .asfreq()
