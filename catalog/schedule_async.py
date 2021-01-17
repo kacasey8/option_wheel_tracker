a571a4451fb85c4be70283024f4832af747d4e76
Kevin Casey <kacasey@fb.com>
2021-01-17 15:10:51 -0800

    fix dev

--- catalog/schedule_async.py
+++ catalog/schedule_async.py
@@ -14,11 +14,8 @@ def schedule_global_put_comparison_async():
   is_currently_running = cache.get(GLOBAL_PUT_RUNNING_CACHE_KEY)
   if is_currently_running is None:
     cache.set(GLOBAL_PUT_RUNNING_CACHE_KEY, True, GLOBAL_PUT_TIMEOUT_SECONDS)
-    try:
-      q = Queue(connection=conn)
-      return q.enqueue(_run_global_put_comparison)
-    except:
-      return False
+    q = Queue(connection=conn)
+    return q.enqueue(_run_global_put_comparison)
   return True
 
 def _run_global_put_comparison():
