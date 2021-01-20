4ae04c4c65d45f27ded08577da4bdc01562a2dec
Kevin Casey <kacasey@fb.com>
2021-01-20 13:46:21 -0800

    faster business days

--- catalog/business_day_count.py
+++ catalog/business_day_count.py
@@ -1,16 +1,8 @@
-import pandas
-from pandas.tseries.holiday import USFederalHolidayCalendar
+import numpy
 
 # Counts stock open days between the two dates, assuming both
 # end days are included
 def busday_count_inclusive(start_date, end_date):
-  C = pandas.offsets.CustomBusinessDay(calendar=USFederalHolidayCalendar())
   if (start_date == end_date):
   	return 1
-  pandas_result = (pandas.DataFrame(index=pandas.to_datetime([start_date, end_date]))
-      .resample(C, closed='right') 
-      .asfreq()
-      .index
-      .size)
-  
-  return pandas_result
+  return np.busday_count(start_date, end_date) + 1
