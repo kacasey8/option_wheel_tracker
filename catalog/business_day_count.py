5c7410be189c9da35e08ab65fb57add21b8fa796
Ryan Casey <rcaseyatbat@gmail.com>
2022-11-11 09:59:48 -0800

    fix typing for bus days

--- catalog/business_day_count.py
+++ catalog/business_day_count.py
@@ -1,9 +1,11 @@
+from datetime import date
+
 import numpy
 
 
 # Counts stock open days between the two dates, assuming both
 # end days are included
-def busday_count_inclusive(start_date, end_date):
+def busday_count_inclusive(start_date: date, end_date: date) -> int:
     if start_date == end_date:
         return 1
-    return numpy.busday_count(start_date, end_date) + 1
+    return int(numpy.busday_count(start_date, end_date)) + 1
